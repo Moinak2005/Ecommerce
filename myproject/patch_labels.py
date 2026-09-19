398cf1d1625b3fea64421f3a9ed5ec5250685e14
Moinak2005 <147593087+Moinak2005@users.noreply.github.com>
2026-09-19 11:34:44 +0530

    Deleted unnecessary files

--- myproject/patch_labels.py
+++
@@ -1,8 +0,0 @@
-with open('myapp/templates/orders.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('<div class="step-name">Processing</div>', '<div class="step-name">Shipped </div>')
-content = content.replace('<div class="step-name">Shipped</div>', '<div class="step-name">Out for delivery</div>')
-
-with open('myapp/templates/orders.html', 'w', encoding='utf-8') as f:
-    f.write(content)
--- myproject/test_products.py
+++
@@ -1,7 +0,0 @@
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-django.setup()
-
-from myapp.models import Product
-print([p.name for p in Product.objects.all()])
